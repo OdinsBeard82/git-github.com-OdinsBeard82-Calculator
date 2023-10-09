24fef9a93ae0cd66a1bb4f30dfae69f77fb7f44a
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-10-09 13:45:33 +0100

    corrected save

--- script.js
+++ script.js
@@ -1,73 +1,18 @@
-const answer = document.querySelector('.answer');
+const answerDiv = document.querySelector('.answer');
+
 const buttons = document.querySelectorAll('button');
 
-let currentAnswer = '0';
-let operator = '';
-let previousNumber = '';
-let currentNumber = '';
-
-function updateDisplay() {
-    answer.textContent = currentAnswer;
-}
-
-function clear() {
-    currentAnswer = '0';
-    operator = '';
-    previousNumber = '';
-    currentNumber = '';
-}
-
-function calculate() {
-    switch (operator) {
-        case '+':
-            currentAnswer = String(Number(previousNumber) + Number(currentNumber));
-            break;
-        case '-':
-            currentAnswer = String(Number(previousNumber) - Number(currentNumber));
-            break;
-        case 'x':
-            currentAnswer = String(Number(previousNumber) * Number(currentNumber));
-            break;
-        case '/':
-            currentAnswer = String(Number(previousNumber) / Number(currentNumber));
-            break;
-        case '%':
-            currentAnswer = String(Number(previousNumber) % Number(currentNumber));
-            break;
-    }
-}
-
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (button.classList.contains('add')) {
-            operator = '+';
-            previousNumber = currentAnswer;
-            currentAnswer = '';
-        }
-        else if (button.classList.contains('subtract')) {
-            operator = '-';
-            previousNumber = currentAnswer;
-            currentAnswer = '';
-        }
-        else if (button.classList.contains('multipy')) {
-            operator = 'x';
-            previousNumber = currentAnswer;
-            currentAnswer = '';
-        }
-        else if (button.classList.contains('divide')) {
-            operator = '/';
-            previousNumber = currentAnswer;
-            currentAnswer = '';
-        }
-        else if (button.classList.contains('%')) {
-            operator = '%';
-            previousNumber = currentAnswer;
-            currentAnswer = '';
-        }
-        else if (button.classList.contains('equals')) {
-            currentNumber = currentAnswer;
-            calculate();
-            operator = '';
-            previousNumber = '';
-            currentNumber = '';
-       
+button.addEventListener('click', () => {
+if (button.value === '=') {
+answerDiv.textContent = eval(answerDiv.textContent);
+} else if (button.value === 'AC') {
+answerDiv.textContent = '0';
+} else {
+if (answerDiv.textContent === '0') {
+answerDiv.textContent = '';
+}
+answerDiv.textContent += button.value;
+}
+});
+});
